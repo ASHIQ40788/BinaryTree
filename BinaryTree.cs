1d8418c4b8b136a12ddba6cd96ffbdc661a981d9
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-23 14:01:14 +0530

    [ASHIQ] added : Insert Node in Binary Tree

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -6,11 +6,73 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
-    class BinaryTree
-    { 
-        //Welcome to Binary Search Tree.
+        public class MyBinaryTree<T> where T : IComparable
+        {
+            public T nodeData { get; set; }
+            public MyBinaryTree<T> leftTree { get; set; }
+            public MyBinaryTree<T> rightTree { get; set; }
+            public static int leftCount;
+            public static int rightCount;
 
-    }
-}
+            public MyBinaryTree(T nodeData)
+            {
+                this.nodeData = nodeData;
+                this.leftTree = null;
+                this.rightTree = null;
+
+            }
+            /// <summary>
+            /// UC1- Inserting a Node.
+            /// </summary>
+            /// <param name="item"></param>
+            public void Insert(T item)
+            {
+                T currentNodeValue = this.nodeData;
+                int value = currentNodeValue.CompareTo(item);
+                if ((value) > 0)
+                {
+                    if (this.leftTree == null)
+                    {
+                        this.leftTree = new MyBinaryTree<T>(item);
+                        Console.WriteLine("Inserting" + item);
+                    }
+                    else
+                        this.leftTree.Insert(item);
+                }
+                else
+                {
+                    if (this.rightTree == null)
+                    {
+                        this.rightTree = new MyBinaryTree<T>(item);
+                        Console.WriteLine("Inserting " + item);
+                    }
+                    else
+                    {
+                        this.rightTree.Insert(item);
+                        Console.WriteLine("Inserting " + item);
+                    }
+
+                }
+
+            }
+
+            public void Display()
+            {
+                if (this.leftTree != null)
+                {
+                    leftCount++;
+                    this.leftTree.Display();
+                }
+
+                Console.WriteLine(this.nodeData.ToString());
+                if (this.rightTree != null)
+                {
+                    rightCount++;
+                    this.rightTree.Display();
+                }
+
+            }
+        }
+ }
 
           
--- Program.cs
+++ Program.cs
@@ -10,10 +10,18 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
+            //UC1-Inserting a Node in Binary Search Tree.
+            MyBinaryTree<int> myBinaryTree = new MyBinaryTree<int>(56);
+            myBinaryTree.Insert(30);
+            myBinaryTree.Insert(70);
+            myBinaryTree.Insert(22);
+            myBinaryTree.Insert(40);
+            myBinaryTree.Insert(95);
 
-            //Welcome to Binary Search Tree.
-            
-           
+            myBinaryTree.Display();
+            Console.ReadLine();
+
+
         }
     }
 }
